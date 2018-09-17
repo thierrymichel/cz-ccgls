277940358e913c76d3eb189e8c1238820ed6a6c5
Thierry Michel <thmichel@gmail.com>
2018-09-17 15:05:35 +0200

    refactor(root): :sparkles: Rewrite with optional flags and separate emoji

---
+++ NOTES.md
@@ -0,0 +1,11 @@
+# Notes
+
+> If emoji can not start the headline (to follow the specs), what about splitting type and gitmoji ?
+> And just keeping the autocomplete ?…and/or grouping them? [choose a visual description]
+
+- [ ] fetch from gitmoji repo
+- [ ] use cache?
+- [ ] commit message length ? (max 100)
+- [ ] add `imp` for `improvement` to [types](conventional-commit-types)
+- [ ] add gitmoji autocomplete for commit message?
+- [ ] is it possible to put gitmoji first? (being compliant with `conventionalCommits` tools)
--- index.js
+++ index.js
@@ -10,13 +10,24 @@ const rightPad = require('right-pad');
 const truncate = require('cli-truncate');
 const wrap = require('wrap-ansi');
 
-const { types } = require('conventional-commit-types');
-const emojis = require('./emojis');
+const { types: conventionalTypes } = require('conventional-commit-types');
+const gitmojis = require('./gitmojis');
 
 const cwd = process.cwd();
 const defaultConfig = {
+  // types: {},
+  additionalTypes: {
+    imp: {
+      description: 'Improves a current implementation without adding a new feature or fixing a bug',
+      title: 'Improvement',
+    },
+  },
+  useEmojis: true,
+  // emojis: [],
+  additionalEmojis: [],
   useScopes: true,
-  scopes: [],
+  // scopes: [],
+  additionalScopes: [],
 };
 
 /**
@@ -56,71 +67,85 @@ function loadConfig() {
       ...defaultConfig,
       ...config,
     }))
-    // Load Lerna scopes if configured
-    .then(config => {
-      if (!config.useScopes) {
-        return config;
-      }
-
-      return Promise.resolve()
-        .then(() => {
-          const Project = importFrom(cwd, '@lerna/project');
-          const project = new Project(cwd);
-
-          return project.getPackages();
-        })
-        .then(packages => ({
-          ...config,
-          scopes: [
-            ...config.scopes,
-            ...packages
-              .map(pkg => pkg.name)
-              .map(name => name.charAt(0) === '@' ? name.split('/')[1] : name),
-          ],
-        }));
-    })
     .catch(() => defaultConfig);
 }
 
-/**
- * Get types for prompt
- * name: <emoji-symbol> <type>: <description>
- * value: <type>
- * emoji: <emoji-code>
- *
- * @returns {array} list of types
- */
-function getTypes() {
-  // Needed for alignment wit rightPad
-  const length = longest(Object.keys(types)).length + 2;
+// eslint-disable-next-line require-jsdoc
+async function loadOptions(config) {
+  const options = {};
 
-  return map(types, (type, key) => {
-    const { code, symbol } = emojis[key];
-    const { description } = type;
+  // Types
+  let types = config.types || conventionalTypes;
 
-    return {
-      name: `${rightPad(`${symbol} ${key}:`, length + symbol.length)} ${description}`,
-      value: key,
-      emoji: code,
+  if (config.additionalTypes) {
+    types = {
+      ...types,
+      ...config.additionalTypes,
     };
-  });
+  }
+
+  options.types = formatTypes(types);
+
+  // Additional types
+  // if (config.additionalTypes) {
+  //   options.types = {
+  //     ...options.types,
+  //     ...config.additionalTypes,
+  //   };
+  // }
+
+  // Gitmoji
+  if (config.useEmojis) {
+    let emojis = config.emojis || gitmojis;
+
+    if (config.additionalEmojis) {
+      emojis = [
+        ...emojis,
+        ...config.additionalEmojis,
+      ];
+    }
+
+    options.emojis = formatEmojis(emojis);
+  }
+
+  // Load Lerna scopes if configured
+  if (config.useScopes) {
+    if (config.scopes) {
+      options.scopes = config.scopes;
+    } else {
+      const Project = importFrom(cwd, '@lerna/project');
+      const project = new Project(cwd);
+      const packages = await project.getPackages();
+
+      options.scopes = packages
+        .map(pkg => pkg.name)
+        .map(name => name.charAt(0) === '@' ? name.split('/')[1] : name);
+    }
+
+    options.scopes = [
+      ...options.scopes,
+      ...config.additionalScopes,
+    ];
+  }
+
+  return options;
 }
 
 /**
  * Fill prompt for questions
  * 1. Type
  * 2. Scope
- * 3. Subject (short)
- * 4. Issues
- * %. Body (long)
+ * 3. Gitmoji
+ * 4. Subject (short)
+ * 5. Issues
+ * 6. Body (long)
  *
- * @param {object} config from `loadConfig`
+ * @param {object} options from `loadOptions`
  * @return {array} list of questions
  */
-function fillPrompt(config) {
-  const choices = getTypes();
-
-  return [
+function fillPrompt(options) {
+  const { types, scopes, emojis } = options;
+  const prompts = [
     {
       type: 'autocomplete',
       name: 'type',
@@ -130,7 +155,7 @@ function fillPrompt(config) {
         const query = input || '';
 
         return new Promise(resolve => {
-          const result = fuzzy.filter(query, choices, {
+          const result = fuzzy.filter(query, types, {
             extract: el => el.name,
           });
 
@@ -141,10 +166,10 @@ function fillPrompt(config) {
       },
     },
     {
-      type: config.scopes ? 'list' : 'input',
+      type: scopes ? 'list' : 'input',
       name: 'scope',
       message: 'Specify a scope:',
-      choices: config.scopes && [
+      choices: scopes && [
         {
           name: '[none]',
           value: '',
@@ -153,7 +178,7 @@ function fillPrompt(config) {
           name: 'root',
           value: 'root',
         },
-      ].concat(config.scopes),
+      ].concat(scopes),
     },
     {
       type: 'input',
@@ -171,6 +196,29 @@ function fillPrompt(config) {
       message: 'Provide a longer description:',
     },
   ];
+
+  if (emojis) {
+    prompts.splice(2, 0, {
+      type: 'autocomplete',
+      name: 'emoji',
+      message: 'Choose an emoji:',
+      source: (answersSoFar, input) => {
+        const query = input || '';
+
+        return new Promise(resolve => {
+          const result = fuzzy.filter(query, emojis, {
+            extract: el => el.name,
+          });
+
+          setTimeout(() => {
+            resolve(result.map(el => el.original));
+          }, 100);
+        });
+      },
+    });
+  }
+
+  return prompts;
 }
 
 /**
@@ -182,9 +230,11 @@ function fillPrompt(config) {
 function format(answers) {
   // Optional scope with parenthesis
   const scope = answers.scope ? `(${answers.scope.trim()})` : '';
+  // Optional subject with emoji
+  const subject = answers.emoji ? `${answers.emoji} ${answers.subject}` : '';
 
   // Build head line, add emoji and limit to 100
-  const head = truncate(`${answers.type}${scope}: ${emojis[answers.type].code} ${answers.subject.trim()}`, 100);
+  const head = truncate(`${answers.type}${scope}: ${subject.trim()}`, 100);
   const body = wrap(answers.body, 100);
   const footer = (answers.issues.match(/#\d+/g) || [])
     .map(issue => `Closes ${issue}`)
@@ -202,9 +252,52 @@ module.exports = {
   prompter(cz, commit) {
     cz.prompt.registerPrompt('autocomplete', autocomplete);
     loadConfig()
+      .then(loadOptions)
       .then(fillPrompt)
       .then(cz.prompt)
       .then(format)
       .then(commit);
   },
 };
+
+/**
+ * Get types for prompt
+ * name: <type>: <description>
+ * value: <type>
+ * @param {object} types types to format
+ * @returns {array} list of formatted types
+ */
+function formatTypes(types) {
+  // Needed for alignment wit rightPad
+  const length = longest(Object.keys(types)).length + 2;
+
+  return map(types, (item, key) => {
+    const { description } = item;
+
+    return {
+      name: `${rightPad(`${key}:`, length)} ${description}`,
+      value: key,
+    };
+  });
+}
+
+/**
+ * Get emojis for prompt
+ * name: <symbol>: <description>
+ * value: <code>
+ * @param {array} emojis emojis to format
+ * @returns {array} list of formatted emojis
+ */
+function formatEmojis(emojis) {
+  // Needed for alignment wit rightPad
+  // const length = longest(Object.keys(types)).length + 2;
+
+  return emojis.map(item => {
+    const { emoji, code, description } = item;
+
+    return {
+      name: `${emoji}: ${description}`,
+      value: code,
+    };
+  });
+}
